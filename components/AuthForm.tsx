e154ff5034b0bb32dd8f3e5bb495e620302dfdcd
FixCrap <george_andriopoulos@hotmail.gr>
2024-11-10 20:53:42 +0200

    Implemented User action create and onSubmite function (not full)

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { z } from "zod";
+import { set, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
@@ -17,6 +17,7 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -33,6 +34,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [errorMessage, setErrorMessage] = useState("");
+	const [accountId, setAccountId] = useState(null);
 
 	const formSchema = authFormSchema(type);
 
@@ -45,7 +47,20 @@ const AuthForm = ({ type }: { type: FormType }) => {
 	});
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		console.log(values);
+		setIsLoading(true);
+		setErrorMessage("");
+
+		try {
+			const user = await createAccount({
+				fullName: values.fullName || "",
+				email: values.email,
+			});
+			setAccountId(user.accountId);
+		} catch {
+			setErrorMessage("Failed to create account, please try again");
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
 	return (
