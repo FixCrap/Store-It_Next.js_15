e195e249354a9bb71eb58cd4c0e63bc4a3ba894d
FixCrap <george_andriopoulos@hotmail.gr>
2024-11-14 13:49:10 +0200

    Implemented Mobile Navigation and user log in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
 	return (
 		<div className='flex-center '>
-			<h1 className='text-3xl font-bold   text-brand '>
+			<h1 className='text-brand text-3xl   font-bold '>
 				StoreIt - The only storage solution you need.
 			</h1>
 		</div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { Search } from "lucide-react";
-import FileUploader from "./FileUploader";
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
-const Header = () => {
+const Header = ({
+	userId,
+	accountId,
+}: {
+	userId: string;
+	accountId: string;
+}) => {
 	return (
 		<header className='header'>
 			<Search />
 			<div className='header-wrapper'>
-				<FileUploader />
-				<form>
+				<FileUploader
+					ownerId={userId}
+					accountId={accountId}
+				/>
+				<form
+					action={async () => {
+						"use server";
+
+						await signOutUser();
+					}}>
 					<Button
 						type='submit'
 						className='sign-out-button'>
@@ -27,5 +42,4 @@ const Header = () => {
 		</header>
 	);
 };
-
 export default Header;
